43a3619d1a1a62878bb6c5269af197adea890d8f
JoaoVictorRj <joaovictor_rj@poli.ufrj.br>
2017-05-15 13:33:43 -0300

    implement cast and uncast to void*

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
-#include "ufrjmatrix.hpp"
+#include "matrix.hpp"
+#include <iostream>
+#include <typeinfo>
 
 int main(){
 
-    Matrix<int> mat1(2,2);
-    mat1[0][0] = 13;
-    mat1[0][1] = 2;
-    mat1[1][0] = 4;
-    mat1[1][1] = 23;
-
-    std::cout << mat1 << std::endl;
-
-    mat1.setWidth(3);
-
-    std::cout << mat1 << std::endl;
+    Matrix<int> mat1(3,3,11);
     
+    std::cout << mat1 << std::endl;
+
+    void *pointer = (void*)mat1;
+
+
+    mat1[1][1] = 3;
+    
+    Matrix<int> mat2;
+
+    mat2 = (Matrix<int>)mat1;
+
+    std::cout << "------------" << std::endl << mat2 << std::endl;
+
     return 0;
 }
